6f7b82137b274e9eee2243af0679ed873ba309f2
davidroman-hub <jobroman83@gmail.com>
2020-01-27 15:55:04 +0100

    using hookds

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
-class App extends Component {
+// class App extends Component {
 
-state = { 
-  count: 0
-};
+// state = { 
+//   count: 0
+// };
 
-increment = () => {
-  this.setState({
-   count: this.state.count + 1
-  });
-} ;
+// increment = () => {
+//   this.setState({
+//    count: this.state.count + 1
+//   });
+// } ;
 
-  render(){
+//   render(){
+//     return(
+//       <div>
+//         <h2> Counter App</h2>
+//           <button  onClick={this.increment}>Click {this.state.count} times </button>
+//       </div>
+//     )
+//   }
+// }
+
+const App = () => {
+      const [count, setCount] = useState(0)
+
+      const increment = () => {
+        setCount(count + 1)
+      } 
+
     return(
-      <div>
-        <h2> Counter App</h2>
-          <button  onClick={this.increment}>Click {this.state.count} times </button>
-      </div>
+     <div>
+          <h2> Counter App</h2>
+    <button onClick={ increment }> Clicked {count} times</button> 
+
+     </div> 
     )
-  }
+
 }
 
 export default App;
