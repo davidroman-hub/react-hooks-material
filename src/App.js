a3f76756c23821cc7a3b069a906cc9fa61cec47e
davidroman-hub <jobroman83@gmail.com>
2020-01-28 14:25:10 +0100

    adding funtion to the button

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ const App = () => {
 //            setState
   const [news,setNews ] = useState ([])
   const [searchQuery, setSearchQuery] = useState ('react') 
+  const [url, setUrl] = useState ('http://hn.algolia.com/api/v1/search?query=react ') // by default we gonna use this url
   //Method fetch news
 
   const fetchNews = () => {
-    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    fetch(url)
     .then(result => result.json() )
     //.then(data=> console.log(data));
     .then(data => setNews(data.hits)) //data hits for one by one (20 news in array)
@@ -21,16 +22,23 @@ const App = () => {
   /// we need to run this method and we gonna used with useEffect
   useEffect(() => {
       fetchNews();
-  },[searchQuery]);
+  },[url]);
 
   const handleChange = e => {
     setSearchQuery(e.target.value)
+  };
+  const handleSubmit = e => { 
+    e.preventDefault()
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`
+    )
   }
+
+
 
   return(
     <div>
       <h2>News</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" value={searchQuery} onChange={handleChange}/>
         <button>Search</button>
       </form>
